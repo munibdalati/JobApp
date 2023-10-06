b057ddabcb0b5e110587f2e46e882cdc52e104b1
Munib Al Dalati <munib@Munibs-MacBook-Pro.local>
2023-10-06 21:46:02 +0300

    edits

--- Back-End/index.js
+++ Back-End/index.js
@@ -6,37 +6,29 @@ const applicationRoutes = require("./routes/applicationRoutes");
 const vacancyRoutes = require("./routes/vacancyRoutes");
 
 const cors = require("cors");
-// const path = require("path");
-
-
+const path = require("path");
 
 // express app
 const app = express();
 
 // Configure CORS
-app.use(cors(
-  {
-    origin:['https://deploy-mern-1whq.vercel.app'],
-    methods:['GET', 'POST'],
-    credentials:true
-  }
-));
+app.use(cors());
 
 // Connect DB
 connectDB();
 
 // Middleware
 app.use(express.json());
-// app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.static(path.join(__dirname + "/public")));
 
 // Routes
-// app.use(function(req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use("/api/application", applicationRoutes);
 app.use("/api/vacancy", vacancyRoutes);
