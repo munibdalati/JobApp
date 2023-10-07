46e62858515605392012391a614a990785972db8
Munib Al Dalati <munib@Munibs-MacBook-Pro.local>
2023-10-07 14:29:19 +0300

    edits

--- Back-End/index.js
+++ Back-End/index.js
@@ -38,7 +38,7 @@ app.use("/api/vacancy", vacancyRoutes);
 
 
 // Port
-const PORT =  process.env.PORT;
+const PORT = 8000 ||  process.env.PORT;
 
 const server = app.listen(PORT , () =>
   console.log(`Server running on port ${PORT}`)
